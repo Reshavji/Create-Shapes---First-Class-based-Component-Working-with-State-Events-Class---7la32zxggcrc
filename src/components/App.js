5f869eda65e3623bfeeff3088860fe286bf4f517
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-05-04 22:43:20 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  return (
+    <div id="main">
+      <div id="shape-creator">
+        
+
+      </div>
+      <div id="shapes-holder">
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
